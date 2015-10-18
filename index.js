28d2f89bca2359cb1ca372db94566c2c4592284f
Tom <tom@jackhq.com>
2015-10-17 21:47:05 -0400

    update with response error for timeout

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+var responseError = require('palmettoflow-event').responseError
+
 module.exports = function (config, ee) {
   return function (ne, callback) {
     var timeoutHasRun = false
     var to = setTimeout(function () {
       timeoutHasRun = true
-      cb({ message: 'Timeout has occured for: ' + event.to })
+      cb(null, responseError(ne,
+        { message: 'Timeout has occured for: ' + event.to })
+      )
     }, config.timeout || 1000)
     ee.on(ne.from, function (event) {
       if (!timeoutHasRun) {
